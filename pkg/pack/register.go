e190cebed89f24d3fc06f4eec35c77f8a337d4ba
lauxinchi <lauxinchi@gmail.com>
2022-11-04 15:10:04 +0800

    Add register packet encode & decode.

--- pkg/pack/register.go
+++ pkg/pack/register.go
@@ -1,5 +1,9 @@
 package pack
 
+import (
+	"errors"
+)
+
 type RegisterPacket struct {
 	*CommonPacket
 	SrcMac  [4]byte
@@ -7,11 +11,29 @@ type RegisterPacket struct {
 }
 
 func (cp *RegisterPacket) Encode() ([]byte, error) {
+	b := make([]byte, 28)
 
-	return nil, nil
+	cmBytes, err := cp.CommonPacket.Encode()
+	if err != nil {
+		return nil, errors.New("invalid common packets")
+	}
+	copy(b[0:20], cmBytes)
+	copy(b[20:24], cp.SrcMac[:])
+	copy(b[24:28], cp.DestMac[:])
+	return b, nil
 }
 
-func (cp *RegisterPacket) Decode() (interface{}, error) {
+func (cp *RegisterPacket) Decode(udpBytes []byte) (interface{}, error) {
 
-	return cp, nil
+	res := &RegisterPacket{}
+	cm := &CommonPacket{}
+	cm, err := cm.Decode(udpBytes[0:20])
+	if err != nil {
+		return nil, errors.New("decode common packets failed")
+	}
+
+	copy(res.SrcMac[:], udpBytes[20:24])
+	copy(res.DestMac[:], udpBytes[25:28])
+	res.CommonPacket = cm
+	return res, nil
 }
