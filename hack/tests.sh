2632d715f1b39d0c8fd88227fa0532b13e23e29f
lauxinchi <lauxinchi@gmail.com>
2023-05-22 11:22:19 +0800

    fix test.

--- hack/tests.sh
+++ hack/tests.sh
@@ -1,5 +1,3 @@
 #/bin/sh
-make build
-make image
-docker run --name registry  -p 3001:3000 -d star:v0.1 star registry
-docker run --name star -p 3000:3000 -d star:v0.1 star edge
+nohup ./fvpn node &
+./fvpn join xxx
