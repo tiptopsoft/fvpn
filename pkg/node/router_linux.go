fae5d123799f6cc8c2141508651c2bf2352da748
lauxinchi <lauxinchi@gmail.com>
2023-07-12 23:52:07 +0800

    fix route

--- pkg/node/router_linux.go
+++ pkg/node/router_linux.go
@@ -16,18 +16,18 @@ func (r *router) RemoveRouter(ip string) error {
 }
 
 func (r *router) action(ip, action string) error {
-	originIP := ip
+	var rule string
 	if !strings.Contains(ip, "/") {
-		ip = fmt.Sprintf("%s/24", ip)
-	}
-
-	_, ipNet, err := net.ParseCIDR(ip)
-	if err != nil {
-		return err
+		rule = fmt.Sprintf("route %s -host %s dev %s", action, ip, r.name)
+	} else {
+		_, ipNet, err := net.ParseCIDR(ip)
+		if err != nil {
+			return err
+		}
+		rule = fmt.Sprintf("route %s -net %s dev %s", action, ipNet.String(), r.name)
 	}
 
 	//route add -net 5.244.24.0 netmask 255.255.255.0 fvpn0
-	rule := fmt.Sprintf("route %s -net %s netmask %s %s", action, originIP, ipNet.Mask, r.name)
 
 	return util.ExecCommand("/bin/sh", "-c", rule)
 }
